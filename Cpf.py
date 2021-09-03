77b06be7762fd2491d25c8a92cde33d4a51dba09
Lucas Correia <88215259+Luskinnn28@users.noreply.github.com>
2021-09-03 15:30:39 -0300

    Create Cpf.py
    Validador Cpf (aula 01) da Alura. (Alura é a plataforma que estou cursando)

---
+++ Cpf.py
@@ -0,0 +1,32 @@
+class Cpf:
+    def __init__(self, documento):
+        documento = str(documento)
+
+        if self.cpfValido(documento):
+            self.cpf = documento
+
+        else:
+            raise ValueError ('CPF inválido!!')
+
+    def __str__(self):
+        return self.formatCpf()
+
+
+    def cpfValido(self, documento):
+
+        if len(documento) == 11:
+
+            return True
+
+        else:
+                return False
+
+
+    def formatCpf(self):
+
+        fatia_um = self.cpf[:3]
+        fatia_dois = self.cpf[3:6]
+        fatia_tres = self.cpf[6:9]
+        fatia_quatro = self.cpf[9:]
+
+        return ('{}.{}.{}-{}'.format(fatia_um, fatia_dois, fatia_tres, fatia_quatro))
